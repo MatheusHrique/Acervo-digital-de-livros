992cbdc8440b08de70f30c135e88368effc35899
MatheusHrique <147109928+MatheusHrique@users.noreply.github.com>
2024-07-10 11:11:13 -0300

    adicionando banco de dados

---
+++ acervoDigital.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE DigitalLibrary;
+USE DigitalLibrary;
+
+CREATE TABLE Cliente(
+id_cliente TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100), 
+senha VARCHAR(50),
+email VARCHAR(100),
+endereco VARCHAR(100),
+telefone VARCHAR(100),
+dataNascimento DATE,
+CPF VARCHAR(14) UNIQUE
+) ENGINE=INNODB;
+
+CREATE TABLE Autor(
+ id_autor TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ nomeCompleto VARCHAR(100),
+ descricao VARCHAR(500),
+ foto VARCHAR(300)
+) ENGINE=INNODB;
+
+CREATE TABLE Livro(
+id_livro TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id_autor TINYINT,
+titulo VARCHAR(100),
+subtitulo VARCHAR(100),
+isbn INT,
+assuntos VARCHAR(200),
+numEdicao INT,
+editora VARCHAR(100),
+localPublicacao VARCHAR(100),
+numPaginas SMALLINT,
+FOREIGN KEY (id_autor) REFERENCES Autor(id_autor)
+) ENGINE=INNODB;
+
